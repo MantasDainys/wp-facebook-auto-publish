aa8cf126220a557dbab79f0ea471cf31bd60f7b1
Mantas Dainys <m.dainys@eana.lt>
2024-12-05 15:44:38 +0200

    wp-facebook-auto-publish

--- wp-facebook-auto-publish.php
+++ wp-facebook-auto-publish.php
@@ -46,11 +46,86 @@ class WPFacebookAutoPublish {
 		add_action( 'save_post', [ $this, 'save_facebook_publish_meta_box_data' ] );
 		add_action( 'admin_notices', [ $this, 'custom_admin_error_notice' ] );
 
+		add_filter( 'plugins_api', [ $this, 'get_plugin_update_info' ], 20, 3 );
+		add_filter( 'pre_set_site_transient_update_plugins', [ $this, 'check_for_plugin_update' ] );
+
 		$this->appId           = get_option( 'wp_facebook_auto_publish_app_id' );
 		$this->appSecret       = get_option( 'wp_facebook_auto_publish_app_secret' );
 		$this->pageAccessToken = get_option( 'wp_facebook_auto_publish_page_access_token' );
 		$this->pageId          = get_option( 'wp_facebook_auto_publish_page_id' );
 
+	}
+
+	function check_for_plugin_update($transient) {
+
+		if (empty($transient->checked)) {
+			return $transient;
+		}
+
+		$plugin_slug = 'wp-facebook-auto-publish/wp-facebook-auto-publish.php';
+		$repo_url = 'https://api.github.com/repos/MantasDainys/wp-facebook-auto-publish/releases/latest';
+
+		$response = wp_remote_get($repo_url);
+
+		if (is_wp_error($response)) {
+			return $transient;
+		}
+
+		$release = json_decode(wp_remote_retrieve_body($response), true);
+
+		if (isset($release['tag_name'])) {
+			$remote_version = ltrim($release['tag_name'], 'v');
+			$current_version = '1.0.1';
+
+			if (version_compare($remote_version, $current_version, '>')) {
+				$plugin_data = get_plugin_data(WP_PLUGIN_DIR . '/' . $plugin_slug);
+
+				$transient->response[$plugin_slug] = (object) [
+					'slug' => $plugin_slug,
+					'new_version' => $remote_version,
+					'package' => $release['zipball_url'],
+					'url' => $release['html_url'],
+				];
+			}
+		}
+
+		return $transient;
+	}
+
+	function get_plugin_update_info($res, $action, $args) {
+		if ($action !== 'plugin_information') {
+			return $res;
+		}
+
+		if ($args->slug !== 'wp-facebook-auto-publish') {
+			return $res;
+		}
+
+		$repo_url = 'https://api.github.com/repos/MantasDainys/wp-facebook-auto-publish/releases/latest';
+
+		$response = wp_remote_get($repo_url);
+
+		if (is_wp_error($response)) {
+			return $res;
+		}
+
+		$release = json_decode(wp_remote_retrieve_body($response), true);
+
+		if (isset($release['tag_name'])) {
+			$res = (object) [
+				'name' => 'WP Facebook Auto Publish',
+				'slug' => 'wp-facebook-auto-publish',
+				'version' => ltrim($release['tag_name'], 'v'),
+				'author' => '<a href="https://github.com/MantasDainys">Mantas Dainys</a>',
+				'homepage' => 'https://github.com/MantasDainys/wp-facebook-auto-publish',
+				'download_link' => $release['zipball_url'],
+				'sections' => [
+					'description' => 'Automatically publish posts to Facebook.',
+				],
+			];
+		}
+
+		return $res;
 	}
 
 	public function add_facebook_publish_meta_box() {
